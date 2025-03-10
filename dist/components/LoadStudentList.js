5a4901b27b02dcb6d7cf744836a75c7ebddf34e9
Brian <53064162+bmagee88@users.noreply.github.com>
2025-03-09 23:25:46 -0400

    stop tracking dist folder

--- dist/components/LoadStudentList.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var jsx_runtime_1 = require("react/jsx-runtime");
-var material_1 = require("@mui/material");
-var LoadStudentList = function () {
-    return (0, jsx_runtime_1.jsx)(material_1.Box, {});
-};
-exports.default = LoadStudentList;
--- dist/components/RewardButton.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var jsx_runtime_1 = require("react/jsx-runtime");
-var material_1 = require("@mui/material");
-var RewardButton = function () {
-    return (0, jsx_runtime_1.jsx)(material_1.Button, { children: "reward" });
-};
-exports.default = RewardButton;
